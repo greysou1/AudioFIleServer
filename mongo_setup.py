f76660878b9b4d18deb329650b752f5458c1f2a9
GreySou1 <prudvi.kamtam16@gmail.com>
2021-04-07 00:43:52 +0530

    updated

--- mongo_setup.py
+++
@@ -1,5 +0,0 @@
-import mongoengine
-host = 'mongodb+srv://greysou1:KamN1ltam@cluster0.w5sbq.mongodb.net/audio_server?retryWrites=true&w=majority'
-def global_init():
-    mongoengine.register_connection(alias='core', name='audio_server', host=host)
-    
